1ca692ed42372cab8e315e6056f89835d6c0d552
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2026-09-17 05:33:15 -0400

    v2

---
+++ v2/US-Headers/US-Headers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  US-Headers
+//
+//  Created by jht2 on 9/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ v2/US-Headers/US-Headers/US_HeadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  US_HeadersApp.swift
+//  US-Headers
+//
+//  Created by jht2 on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct US_HeadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
